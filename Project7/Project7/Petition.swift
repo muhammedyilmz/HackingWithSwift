902e7846a2d1948b910637ebd993eb3d64bdf70a
Muhammed Yılmaz <yilmaz34muhammed@icloud.com>
2024-11-16 23:29:05 +0300

    Add files via upload

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Muhammed Yılmaz on 14.11.2024.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
